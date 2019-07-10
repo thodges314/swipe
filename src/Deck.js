18182ad8c652914158155843b8293ab72f1e700e
Thomas Hodges <thodges@javascriptonline.org>
2019-07-10 16:29:43 -0700

    list renders

---
+++ src/Deck.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Deck = ({ data, renderCard }) => {
+  const renderCards = () => data.map(item => renderCard(item))
+
+  return (
+    <View>
+      {renderCards()}
+    </View>
+  )
+}
+
+export default Deck
