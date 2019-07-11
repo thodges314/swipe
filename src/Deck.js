4abe95e36a3d79698f2a4a19c625adc9f9c36200
Thomas Hodges <thodges@javascriptonline.org>
2019-07-10 17:41:42 -0700

    rotate on swipe

--- src/Deck.js
+++ src/Deck.js
@@ -2,15 +2,41 @@ import React from 'react'
 import { Animated, PanResponder, View } from 'react-native'
 
 const Deck = ({ data, renderCard }) => {
-  const renderCards = () => data.map(item => renderCard(item))
+  const getCardStyle = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+    return (
+      {
+        ...position.getLayout(),
+        transform: [{ rotate }]
+      }
+    )
+  }
+  const renderCards = () => data.map((item, index) => {
+    if (index === 0) {
+      return (
+        <Animated.View
+          key={item.id}
+          style={getCardStyle()}
+          {...panResponder.panHandlers}
+        >
+          {renderCard(item)}
+        </Animated.View>
+      )
+    }
+    return renderCard(item)
+  })
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (event, gesture) => { console.log(gesture) },
+    onPanResponderMove: (event, gesture) => { position.setValue({ x: gesture.dx, y: gesture.dy }) },
     onPanResponderRelease: () => {}
   })
+  const position = new Animated.ValueXY()
 
   return (
-    <View {...panResponder.panHandlers}>
+    <View>
       {renderCards()}
     </View>
   )
