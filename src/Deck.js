74849d5ebd3326994b360cf1d6e31e3503213a7c
Thomas Hodges <thodges@javascriptonline.org>
2019-07-11 08:42:41 -0700

    swipes left and right, code reuse

--- src/Deck.js
+++ src/Deck.js
@@ -4,8 +4,17 @@ import {
 } from 'react-native'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_OUT_DURATION = 250
+const SWIPE_THRESHOLD = Dimensions.get('window').width / 4
 
 const Deck = ({ data, renderCard }) => {
+  const forceSwipe = (direction) => {
+    const toXValue = (direction === 'right') ? SCREEN_WIDTH : -1 * SCREEN_WIDTH
+    Animated.timing(position, {
+      toValue: { x: toXValue, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start()
+  }
   const getCardStyle = () => {
     const rotate = position.x.interpolate({
       inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
@@ -40,7 +49,15 @@ const Deck = ({ data, renderCard }) => {
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (event, gesture) => { position.setValue({ x: gesture.dx, y: gesture.dy }) },
-    onPanResponderRelease: () => { resetPosition() }
+    onPanResponderRelease: (event, gesture) => {
+      if (gesture.dx > SWIPE_THRESHOLD) {
+        forceSwipe('right')
+      } else if (gesture.dx < -1 * SWIPE_THRESHOLD) {
+        forceSwipe('left')
+      } else {
+        resetPosition()
+      }
+    }
   })
   const position = new Animated.ValueXY()
 
